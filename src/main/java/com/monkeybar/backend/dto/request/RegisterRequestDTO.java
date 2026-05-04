bc3c6998ecdc56fa084733b387b89db6e15336c2
Andrei Briceag <andrei98briceag@gmail.com>
2026-05-04 17:44:48 +0200

    ImplementedDTO, Repository , Security Start Config

---
+++ src/main/java/com/monkeybar/backend/dto/request/RegisterRequestDTO.java
@@ -0,0 +1,26 @@
+package com.monkeybar.backend.dto.request;
+
+import com.monkeybar.backend.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class RegisterRequestDTO {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 8)
+    private String password;
+
+    @NotNull
+    private Role role;
+
+    private UUID venueId;
+}
